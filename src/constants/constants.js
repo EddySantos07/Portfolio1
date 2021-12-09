6a6a58eb85105877d931d2d8754961cb68cd5143
Eddy <eagle100065@gmail.com>
2021-12-09 04:58:30 -0500

    fixing pseudo desc for discord bot

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -22,7 +22,7 @@ export const projects = [
   {
     title: "Discord Bot",
     description:
-      "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+      "Using Node.js, Javascript this bot can fetch youtube videos, manage your discordChannel and have other commands with a simple discord bot! ",
     image: "/images/1.png",
     tags: ["Node.js", "Javascript"],
     source: "https://github.com/EddySantos07/DiscordBot",
