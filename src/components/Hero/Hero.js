4461f8c1bf142c7f1ad52fec9ec876f60083ca4c
Eddy <eagle100065@gmail.com>
2021-12-09 04:02:55 -0500

    final commit before deployment

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import { WiAlien } from "react-icons/ai";
 import {
   Section,
   SectionText,
   SectionTitle,
 } from "../../styles/GlobalComponents";
 import Button from "../../styles/GlobalComponents/Button";
+import { SocialIcons } from "../Header/HeaderStyles";
 import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
@@ -15,10 +16,14 @@ const Hero = (props) => (
         Welcome to <br />
         My Personal Portfolio
       </SectionTitle>
-      <SectionText>
-        FullStack Developer 
-      </SectionText>
-      <Button onClick={( ) => window.location="https://www.linkedin.com/in/eddysantostech/"} > Contact me! </Button>
+      <SectionText>FullStack Developer</SectionText>
+      <Button
+        onClick={() =>
+          (window.location = "https://www.linkedin.com/in/eddysantostech/")
+        }
+      >
+        Contact me!{" "}
+      </Button>
     </LeftSection>
   </Section>
 );
